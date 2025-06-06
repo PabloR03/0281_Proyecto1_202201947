f7e693c03be5b7f4afae89174fcb287dd9c0a56f
PabloR03 <pabloa10rodriguez@gmail.com>
2025-06-06 09:23:18 -0600

    Modulo Ram

--- Modulos/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_modules.order := {   echo sysinfos.o; :; } > modules.order
--- Modulos/.sysinfos.mod.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_sysinfos.mod := printf '%s\n'   sysinfos.o | awk '!x[$$0]++ { print("./"$$0) }' > sysinfos.mod
